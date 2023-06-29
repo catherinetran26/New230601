2a1b9c1a63890f57a41d5aa494d09d97b75b255a
catherinetran26 <catherine.tran.pro@gmail.com>
2023-06-29 18:00:09 +0200

    le script principal

--- Tran-test.py
+++ Tran-test.py
@@ -1,1 +1,11 @@
 print("Hello")
+#Le code principal : 
+""" - Créez un dépôt contenant un script python utilisant pandas, qui ouvre le dataset. Et
+affiche quelques statistiques de base sur ce dernier (head, describe,
+groupby(“column”).count). """
+
+
+
+
+
+
