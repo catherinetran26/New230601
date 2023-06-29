35145d775b431818dc320555f3aed0ca5c9aeb9c
catherinetran26 <catherine.tran.pro@gmail.com>
2023-06-29 18:03:54 +0200

    feat1

--- Tran-test.py
+++ Tran-test.py
@@ -5,7 +5,8 @@ affiche quelques statistiques de base sur ce dernier (head, describe,
 groupby(“column”).count). """
 
 
-
-
+#La fonctionnalité spéciale 1
+""" Fonctionnalité 1 : ajouter une fonction qui retourne un dataframe filtré de la base de
+données selon une condition spécifique. """
 
 
